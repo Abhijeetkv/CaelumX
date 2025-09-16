425983ea4b0c01660a6c4be59959dd66a5e4c4ce
Abhijeet kumar <abhijeetkv2@gmail.com>
2025-09-16 12:09:00 +0530

    feat: enhance ClerkProvider configuration with publishableKey and navigate function

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -10,7 +10,11 @@ import { ApplicationProvider } from "@ui-kitten/components";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      navigate={(to) => console.log(`Navigate to ${to}`)}
+    >
       {/* Wrap everything inside ApplicationProvider */}
       <ApplicationProvider {...eva} theme={eva.light}>
         <SafeScreen style={{ flex: 1, backgroundColor: "#0b3e2aff" }}>
